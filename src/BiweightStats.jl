47f48258110fbb264fa1bb9adb0772e5ef5ce3fb
Miles Lucas <mdlucas@hawaii.edu>
2022-06-13 20:35:31 -1000

    add biweighttransform iterator

--- src/BiweightStats.jl
+++ src/BiweightStats.jl
@@ -42,6 +42,50 @@ using Statistics
 
 export location, scale, midvar, midcov, midcor
 
+struct BiweightTransform{D,M}
+    data::D
+    med::M
+    cutoff::M
+end
+
+function BiweightTransform(X; c=6, median=nothing)
+    data = filter(!isnan, X)
+    if isnothing(median)
+        med = Statistics.median(data)
+    else
+        med = median
+    end
+    mad = median!(abs.(data .- med))
+    return BiweightTransform(data, med, c * mad)
+end
+
+Base.IteratorSize(bt::BiweightTransform) = Base.IteratorSize(bt.data)
+Base.IteratorEltype(bt::BiweightTransform) = Base.IteratorEltype(bt.data)
+
+function Base.iterate(bt::BiweightTransform)
+    next = iterate(bt.data)
+    isnothing(next) && return nothing
+    data, state = next
+    return _biweight_iterate(bt, data), state
+end
+
+function Base.iterate(bt::BiweightTransform, state)
+    next = iterate(bt.data, state)
+    isnothing(next) && return nothing
+    data, state = next
+    return _biweight_iterate(bt, data), state
+end
+
+function _biweight_iterate(bt, data)
+    d = data - bt.med
+    u2 = (d / bt.cutoff)^2 # ((x - Mx) / (c * MAD))^2
+    if u2 > 1
+        return zero(d), zero(u2), false
+    end
+
+    return d, u2, true
+end
+
 """
     location(X; c=6, maxiter=10, tol=1e-6)
 
@@ -64,23 +108,20 @@ julia> location(X)
 
 1. [NIST: biweight location](https://www.itl.nist.gov/div898/software/dataplot/refman2/auxillar/biwloc.htm)
 """
-function location(X; c=6, maxiter=10, tol=1e-6)
+function location(X; maxiter=10, tol=1e-6, kwargs...)
     T = eltype(X)
     ystar = zero(T)
     ystar_old = ystar
     num = zero(T)
     den = zero(T)
     for _ in 1:maxiter
-        Δ = X .- ystar
-        mad = median(Iterators.map(abs, Iterators.filter(!isnan, Δ)))
+        itr = BiweightTransform(X; kwargs..., median=ystar)
         num = zero(T)
         den = zero(T)
-        for (y, d) in zip(X, Δ)
-            isnan(d) && continue
-            u2 = (d / (c * mad))^2
-            u2 > 1 && continue
+        for (d, u2, flag) in itr
+            flag || continue
             w = (1 - u2)^2
-            num += w * y
+            num += w * (d + itr.med)
             den += w
         end
         ystar_old = ystar
@@ -93,7 +134,7 @@ end
 """
     scale(X; c=6)
 
-Compute the biweight midvariance of the variable.
+Compute the biweight scale of the variable. This is different than the square-root of the midvariance.
 
 # Examples
 
@@ -101,7 +142,7 @@ Compute the biweight midvariance of the variable.
 julia> X = 10 .* randn(rng, 10) .+ 50;
 
 julia> scale(X)
-9.269099543377779
+8.398407088585783
 ```
 
 # References
@@ -112,25 +153,7 @@ julia> scale(X)
 
 [`midcor`](@ref), [`midvar`](@ref), [`midcov`](@ref)
 """
-function scale(X; c=6)
-    Δ = X .- median(X)
-    mad = median(Iterators.map(abs, Iterators.filter(!isnan, Δ)))
-    # init
-    T = eltype(X)
-    num = zero(T)
-    den = zero(T)
-    n = 0
-    for d in Δ
-        isnan(d) && continue
-        u2 = (d / (c * mad))^2
-        u2 > 1 && continue
-        num += d^2 * (1 - u2)^4
-        den += (1 - u2) * (1 - 5 * u2)
-        n += 1
-    end
-
-    return sqrt(n * num / (den * (den - 1)))
-end
+scale(X; kwargs...) = sqrt(midvar(X; kwargs...))
 
 """
     midvar(X; c=6)
@@ -154,18 +177,15 @@ julia> midvar(X)
 
 [`scale`](@ref), [`midcor`](@ref), [`midcov`](@ref)
 """
-function midvar(X; c=6)
-    Δ = X .- median(X)
-    mad = median(Iterators.map(abs, Iterators.filter(!isnan, Δ)))
+function midvar(X; kwargs...)
+    itr = BiweightTransform(X; kwargs...)
     # init
     T = eltype(X)
     num = zero(T)
     den = zero(T)
     n = 0
-    for d in Δ
-        isnan(d) && continue
-        u2 = (d / (c * mad))^2
-        u2 > 1 && continue
+    for (d, u2, flag) in itr
+        flag || continue
         num += d^2 * (1 - u2)^4
         den += (1 - u2) * (1 - 5 * u2)
         n += 1
@@ -185,7 +205,7 @@ Computes biweight midcovariance between the two vectors. If only one vector is p
 julia> X = 10 .* randn(rng, 10, 2) .+ 50;
 
 julia> midcov(X[:, 1], X[:, 2])
--25.644749715907157
+-27.95570929732296
 
 julia> midcov(X[:, 1]) ≈ midvar(X[:, 1])
 true
@@ -199,32 +219,21 @@ true
 
 [`scale`](@ref), [`midvar`](@ref), [`midcor`](@ref)
 """
-function midcov(X::AbstractVector{V}, Y::AbstractVector{S}; c=6) where {V,S}
-    length(X) == length(Y) || throw(DimensionMismatch("vectors must have same length"))
-    Δx = X .- median(X)
-    Δy = Y .- median(Y)
-    madx = median(Iterators.map(abs, Iterators.filter(!isnan, Δx)))
-    mady = median(Iterators.map(abs, Iterators.filter(!isnan, Δy)))
+function midcov(X::AbstractVector{V}, Y::AbstractVector{S}; kwargs...) where {V,S}
+    itrx = BiweightTransform(X; kwargs...)
+    itry = BiweightTransform(Y; kwargs...)
     # init
     T = promote_type(V, S)
     num = zero(T)
     den1 = zero(T)
     den2 = zero(T)
     n = 0
-    for (dx, dy) in zip(Δx, Δy)
-        (isnan(dx) || isnan(dy)) && continue
-        u2 = (dx / (c * madx))^2
-        v2 = (dy / (c * mady))^2
-        if u2 ≤ 1
-            den1 += (1 - u2) * (1 - 5 * u2)
-        end
-        if v2 ≤ 1
-            den2 += (1 - v2) * (1 - 5 * v2)
-        end
-        if u2 ≤ 1 && v2 ≤ 1
-            num += dx * (1 - u2)^2 * dy * (1 - v2)^2
-            n += 1
-        end
+    for ((dx, u2, flagx), (dy, v2, flagy)) in zip(itrx, itry)
+        (flagx && flagy) || continue
+        num += dx * (1 - u2)^2 * dy * (1 - v2)^2
+        den1 += (1 - u2) * (1 - 5 * u2)
+        den2 += (1 - v2) * (1 - 5 * v2)
+        n += 1
     end
 
     return n * num / (den1 * den2)
@@ -304,7 +313,7 @@ where ``s_{xx},s_{yy}`` are the midvariances of each vector, and ``s_{xy}`` is t
 julia> X = 10 .* randn(rng, 10, 2) .+ 50;
 
 julia> midcor(X[:, 1], X[:, 2])
--0.22014156692229941
+-0.23997947795605037
 ```
 
 # References
